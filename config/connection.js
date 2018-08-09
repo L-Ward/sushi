6d1e88ca2d380186b12a9db635b7d9b028795a82
L-Ward <noreply>
2018-08-09 14:04:52 -0700

    addjustments to database connection for heroku hosting

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "dyud5fa2qycz1o3v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "jmfpulep14ygfyeo",
-  password: "gacwqn4hvm6grbps",
-  database: "zrch1nfe047ri5a3	"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "sushi_db"
+  });
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
